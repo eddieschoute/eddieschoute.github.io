71c1ad2052ac7dad09e9976ef423e23df60e5515
Eddie Schoute <eddieschoute@gmail.com>
2023-11-17 11:59:10 -0700

    Expand all / collapse all functionality

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ $('.collapser').click(function () {
     $(this).parent().next().collapse('toggle');
     $(this).toggleClass('collapsed')
 });
+
+$("#expandAll").on("click", function () {
+    $('.accordion-collapse').collapse('show');
+})
+
+$("#collapseAll").on("click", function () {
+    $('.accordion-collapse').collapse('hide');
+})
