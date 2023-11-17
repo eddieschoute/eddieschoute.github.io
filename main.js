5415561a09f299130f1b77137369f87ba6c410c8
Eddie Schoute <eddieschoute@gmail.com>
2023-11-17 14:53:10 -0700

    Add search and expand/collapse functionality

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
-$('.collapser').click(function () {
+$('.collapser').on("click", function () {
     $(this).parent().next().collapse('toggle');
     $(this).toggleClass('collapsed')
 });
 
 $("#expandAll").on("click", function () {
     $('.accordion-collapse').collapse('show');
-})
+});
 
 $("#collapseAll").on("click", function () {
     $('.accordion-collapse').collapse('hide');
-})
+});
+
+function filterAccordions(text) {
+    // case insensitive search
+    const findText = text.toLowerCase();
+
+    $(".accordion-item").each(function () {
+        const accordion = $(this)
+        // If the accordion item contains the text, show it, in case it was hidden.
+        if (accordion.text().toLowerCase().indexOf(findText) >= 0) {
+            accordion.show(400);
+        } else {
+            accordion.hide(400);
+        }
+    });
+}
+
+$("#search").on("keyup", function () {
+    filterAccordions($(this).val());
+});
+
